2cbff15cd9176eaa14c90231f82f95928e61e3b2
rkambai <rkam002@e.ntu.edu.sg>
2024-03-19 23:54:30 +0800

    added dim reduction to try and make clustering indexing better, still poor performance

--- indexers.py
+++ indexers.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import TruncatedSVD, PCA
 import numpy as np
 
 
@@ -13,20 +14,35 @@ class Indexer(ABC):
 
     @abstractmethod
     def create_indexes(self, data):
+        """
+        creates indexes out of ENCODED data
+        """
         pass
 
     @abstractmethod
     def search_indexes(self, query):
+        """
+        searches indexes for an ENCODED query
+        """
         pass
     
 class LeaderIndexer(Indexer):
-    def __init__(self, n_clusters=5):
+    def __init__(self, n_clusters = 5, use_pca = False, min_explained_var = 0.95):
         self.n_clusters = n_clusters
+        self.use_pca = use_pca
+        self.min_explained_var = min_explained_var
         self.cluster_centers_ = None
         self.clusters_ = None
         self.data_ = None
+        self.dim_reducer_ = None
 
     def create_indexes(self, data: np.ndarray):
+        if self.use_pca:
+            if not isinstance(data, np.ndarray):
+                data = np.array(data)
+            self.dim_reducer_ = self._fit_dim_reducer(data, self.min_explained_var)
+            data = self.dim_reducer_.transform(data)
+
         kmeans = KMeans(n_clusters=self.n_clusters, n_init='auto')
         clusters = kmeans.fit_predict(data)
         self.cluster_centers_ = kmeans.cluster_centers_
@@ -35,6 +51,11 @@ class LeaderIndexer(Indexer):
 
 
     def search_indexes(self, query: np.ndarray):
+        if self.use_pca:
+            if not isinstance(query, np.ndarray):
+                query = np.array(query)
+            query = self.dim_reducer_.transform(query)
+
         ## find index of cluster center most similar to query
         leader_idx = np.argmax(cosine_similarity(self.cluster_centers_, query))
         cluster_indices = np.where(self.clusters_ == leader_idx)
@@ -46,3 +67,5 @@ class LeaderIndexer(Indexer):
         sorted_results = cluster_member_similarities.flatten().argsort()[::-1]
         return sorted_results
     
+    def _fit_dim_reducer(self, data, min_explained_var):
+        return PCA(n_components = min_explained_var).fit(data)
